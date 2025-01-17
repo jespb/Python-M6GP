8743bdad93902a0448940a2610e68a787e7ee575
João Batista <batista-sk@hotmail.com>
2025-01-17 11:25:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 import setuptools
-setuptools.setup()
+
+setup(
+    name="m6gp",  # Replace with your project's name
+    version="1.0.0",  # Replace with your project's version
+    install_requires=[],  # List your dependencies or use requirements.txt
+    author="João Eduardo Batista",
+    author_email="joao.batista@riken.jp",
+    description="Python implementation of the M6GP algorithm, a multi-objective genetic programming algorithm for feature engineering",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jespb/Python-M6GP",
+)
